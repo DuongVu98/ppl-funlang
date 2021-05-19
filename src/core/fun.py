fbd4bc8eb4a4d87e2cbe1eb59f60f9f575011724
DuongVu98 <duongvutung1998@gmail.com>
2021-05-20 00:13:07 +0700

    run from main

--- src/core/fun.py
+++ src/core/fun.py
@@ -172,5 +172,10 @@ class Lexer:
 def run(fn, text):
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
+    if error:
+        return None, error
 
-    return tokens, error
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    return ast.node, ast.error
